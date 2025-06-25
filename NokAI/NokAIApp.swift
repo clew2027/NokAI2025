83769226de9bb0224dc8f6dab62e76f1b9a58b66
clew2027 <clew27@seas.upenn.edu>
2025-06-24 20:18:01 -0400

    Initial Commit

---
+++ NokAI/NokAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NokAIApp.swift
+//  NokAI
+//
+//  Created by Charlotte Lew on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NokAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
